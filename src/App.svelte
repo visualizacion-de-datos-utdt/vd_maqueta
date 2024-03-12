88344f1e9ecbac0ca9f832c768978d7f56be47fd
Carlos Araujo <infocarlosaraujo@gmail.com>
2024-03-12 17:33:02 -0300

    Update chart numbers in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,11 @@
   <div class="chart">
     <div class="item">
       <img style="height: 75px" src="./images/person-iso.svg" alt="person" />
-      <p class="numero">25</p>
+      <p class="numero">40</p>
     </div>
     <div class="item">
       <img style="height: 150px" src="./images/person-iso.svg" alt="person" />
-      <p class="numero">100</p>
+      <p class="numero">80</p>
     </div>
   </div>
 </main>
