cb11aa02cfb542c9258961279db7438995148b6b
Carlos Araujo <infocarlosaraujo@gmail.com>
2024-03-13 07:58:24 -0300

    Remove chart from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,6 @@
 </script>
 
 <!-- Estructura contenido HTML -->
-<main id="container">
-  <h3 class="title">Título del gráfico</h3>
-  <div class="chart">
-    <div class="item">
-      <img style="height: 75px" src="./images/person-iso.svg" alt="person" />
-      <p class="numero">40</p>
-    </div>
-    <div class="item">
-      <img style="height: 150px" src="./images/person-iso.svg" alt="person" />
-      <p class="numero">80</p>
-    </div>
-  </div>
-</main>
 
 <!-- Estilos CSS -->
 <style>
